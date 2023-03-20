5d5fdb7bd1b3005232beb0f30eeb6701e12e5da3
nmk098 <nkuryumov@gmail.com>
2023-03-20 19:57:24 +0400

    add

--- sparrowScroll/ViewController.swift
+++ sparrowScroll/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    let scrollView = UIScrollView()
+    let imageView = UIImageView(image: UIImage(named: "planetExpo"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        
+        scrollView.addSubview(imageView)
+        
+
+        scrollView.frame = view.bounds
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: imageView.bounds.height)
+        scrollView.backgroundColor = .white
+        view.addSubview(scrollView)
+
+        scrollView.maximumZoomScale = 1.5
+        scrollView.minimumZoomScale = 1.0
     }
-
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let imageHeight: CGFloat = 270.0
+        let yOffset = scrollView.contentOffset.y
+        var imageFrame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: imageHeight)
+        if yOffset < 0 {
+            imageFrame.origin.y = yOffset
+            imageFrame.size.height -= yOffset
+        }
+        else {
+            imageFrame.origin.y = -yOffset
+        }
+        scrollView.subviews.first?.frame = imageFrame
+        scrollView.verticalScrollIndicatorInsets.top = imageFrame.height - 45
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
 
